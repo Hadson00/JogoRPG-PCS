bd0920e2f23698390e5646446724fb884e9406e1
Hadson00 <hadson798@gmail.com>
2024-04-03 22:06:26 -0300

    ControlChanges

--- control.py
+++ control.py
@@ -10,14 +10,16 @@ from character import *
 class Control:
     while True:
         print('_________Bem Vindo Ao RPG_________\n')
+
         try:
             selector = (int(input('Digite 1 para criar seu personagem \n'
                                   'Digite 2 para finalizar o jogo \n'
-                                  'Selecione: \n')))
+                                  'Selecione: ')))
             if selector == 1:
                 player_name = str(input('Digite o nome do seu personagem: '))
                 print('--------------------------------')
                 print('_________Escolha sua raça_________\n')
+
                 player_race = (int(input('0: Anão\n'
                                         '1: Elfo\n'
                                         '2: Bárbaro\n'
@@ -26,32 +28,39 @@ class Control:
                                         '5: Gnomo\n'
                                         '6: Ladrão\n'
                                         'Selecione: ')))
+                
                 races = ['Anão', 'Elfo', 'Bárbaro', 'Humano', 'Draconato', 'Gnomo', 'Ladrão'] #Todas as raças
+
                 match player_race: #Implementar player_race em races
                     case 0:
                         print('_________Escolher Sub-Raça_________')
+
                         player_race = (int(input('0: Anão da colina\n'
                                                 '1: Anão da montanha\n'
-                                                '2: Sem troca de raça\n'
+                                                '2: Anão\n'
                                                 'Selecione: ')))
+                        
                         races = ['Anão da colina', 'Anão da montanha','Anão']
-                        match player_race:
+
+                        match player_race: #Implementar Sub-Raça em races
                             case 0:
                                 player_character = HillDwarf(player_name, races[player_race])
                             case 1:
                                 player_character = MountainDwarf(player_name, races[player_race])
                             case 2:
                                 player_character = Dwarf(player_name, races[player_race])
-                            case _:
-                                print('Erro -> Número inválido')
+
                     case 1: 
                         print('\n_________Deseja uma Sub-Raça_________')
+
                         player_race = (int(input('0: Elfo Alto\n'
                                                 '1: Elfo da floresta\n'
                                                 '2: Elfo negro\n'
-                                                '3: Sem troca de raça\n'
+                                                '3: Elfo\n'
                                                 'Selecione: ')))
+                        
                         races = ['Elfo Alto', 'Elfo da floresta','Elfo negro', 'Elfo']
+
                         match player_race:
                             case 0:
                                 player_character = HighElf(player_name, races[player_race])
@@ -61,16 +70,16 @@ class Control:
                                 player_character = DarkElf(player_name, races[player_race])
                             case 3:
                                 player_character = Elf(player_name, races[player_race])
-                            case _:
-                                print('Erro -> Número inválido')
 
                     case 2: 
                         print('_________Escolher Sub-Raça_________')
                         player_race = (int(input('0: Bárbaro Veloz\n'
                                                  '1: Bárbaro Robusto\n'
-                                                 '2: Sem troca de raça\n'
+                                                 '2: Bárbaro\n'
                                                  'Selecione: ')))
+                        
                         races = ['Bárbaro Veloz', 'Bárbaro Robusto','Bárbaro']
+
                         match player_race:
                             case 0:
                                 player_character = Lightfoot(player_name, races[player_race])
@@ -78,8 +87,6 @@ class Control:
                                 player_character = Stout(player_name, races[player_race])
                             case 2:
                                 player_character = Barbarian(player_name, races[player_race])
-                            case _:
-                                print('Erro -> Número inválido')
 
                     case 3: 
                         player_character = Human(player_name, races[player_race])
@@ -89,11 +96,14 @@ class Control:
 
                     case 5: 
                         print('_________Escolher Sub-Raça_________')
+
                         player_race = (int(input('0: Gnomo da floresta\n'
                                                  '1: Gnomo da pedra\n'
-                                                 '2: Sem troca de raça\n'
+                                                 '2: Gnomo\n'
                                                  'Selecione: ')))
+                        
                         races = ['Gnomo da floresta', 'Gnomo da pedra','Gnomo']
+                        
                         match player_race:
                             case 0:
                                 player_character = ForestGnome(player_name, races[player_race])
@@ -101,8 +111,6 @@ class Control:
                                 player_character = RockGnome(player_name, races[player_race])
                             case 2:
                                 player_character = Gnome(player_name, races[player_race])
-                            case _:
-                                print('Erro -> Número inválido')
                         
                     case 6: 
                         player_character = Tiefling(player_name, races[player_race])
@@ -111,9 +119,8 @@ class Control:
 
             elif selector == 2:
                     break
-
             else: 
-                print('Error')
+                print('Error -> Seletor Incorreto')
                 break
         except:
-            print('Erro -> Seletor incorreto')
+            print('Erro -> Seletor Incorreto')
